d745b0459028a7a02f632b853360b59317027ad2
SunhyeokChoe <chltnsgur2@daum.net>
2021-09-26 21:37:14 +0900

    [1.2.0] Add src 폴더 하위 내용 변경시 자동으로 컴파일 되도록 tsc-watch 추가. dist 배포 폴더 생성 및 컴파일 outDir로 지정.

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const sayHi = (name: string, age: number, gender: string): void => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}.`);
+}
+
+sayHi('shchoe', 123, 'male');
+
+export {};
